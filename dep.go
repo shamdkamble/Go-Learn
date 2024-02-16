6b425daf72fd8add3d336b30f9f8cf6bd4720bd1
vsys-shamkamble <sham.kamble@vedanshis.com>
2024-02-16 13:01:21 +0530

    second commit

---
+++ dep.go
@@ -0,0 +1,7 @@
+package test 
+
+func DoSomething()string{
+
+         return "This is imported function from different package !"
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shamdkamble/Go-Learn
+
+go 1.21.6
